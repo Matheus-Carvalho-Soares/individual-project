15a792626caae5b735158cb2b23f063b87b28a4c
Matheus <matheus.csoares@sptech.school>
2025-05-04 00:13:35 -0300

    Estruturação das configurações da API; correção de alguns erros no dimensionamentos na main; ajustes no css.

--- app.js
+++ app.js
@@ -17,14 +17,11 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use(cors());
 
