aff6ebbffed261ff3ecf97a26fc1bfdacba41106
Matheus <matheus.csoares@sptech.school>
2025-05-16 23:59:05 -0300

    projeto com tema novo - jogo Terraria; index feita, cadastro com a primeira parte de seleção de gênero, login feito com API conectada

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,37 +6,13 @@
 comandos para mysql server
 */
 
-CREATE DATABASE hydrogen;
-USE hydrogen;
+CREATE DATABASE Matherraria;
+USE Matherraria;
 
 CREATE TABLE usuario(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-conhecimento TINYINT,
+nome VARCHAR(100),
+genero CHAR(1),
 email VARCHAR(100),
 senha VARCHAR(100),
 dtRegistro DATETIME DEFAULT CURRENT_TIMESTAMP);
-
-CREATE TABLE pontuacao(
-idPontuacao INT AUTO_INCREMENT,
-fkUsuario INT,
-CONSTRAINT fkDep PRIMARY KEY (idPontuacao, fkUsuario),
-CONSTRAINT fk_Usuario FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
-pontos INT,
-dtPontuacao DATETIME DEFAULT CURRENT_TIMESTAMP);
-
-CREATE TABLE dadosHidrogenio(
-idDados INT PRIMARY KEY AUTO_INCREMENT,
-preco INT,
-autonomia INT,
-dtDados DATETIME DEFAULT CURRENT_TIMESTAMP);
-
-INSERT INTO usuario VALUES
-(1, 1, 'usuario123@usuario.com', 'usuario123', DEFAULT);
-
-INSERT INTO pontuacao VALUES
-(1, 1, 50, DEFAULT);
-
-INSERT INTO dadosHidrogenio VALUES
-(1, 6.20, 1200, DEFAULT);
-
-SELECT * from dadosHidrogenio;
