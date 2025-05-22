de2f64e878273912fe8ccdb8de2795297384b232
Matheus <matheus.csoares@sptech.school>
2025-05-22 13:54:46 -0300

    model e controller atualizados, idUsuario armazenado pós login

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -18,7 +18,7 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
-                        res.json(resultadoAutenticar[0])
+                        res.json(resultadoAutenticar[0]);
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
